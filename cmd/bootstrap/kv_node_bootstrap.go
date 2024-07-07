c02f7412add6dc33072c9cef4be1c27dcfc30809
Vlad Ananyev <nexus.riot@gmail.com>
2024-07-07 10:51:06 +0300

    moved bootstrap preview code

--- cmd/bootstrap/kv_node_bootstrap.go
+++ cmd/bootstrap/kv_node_bootstrap.go
@@ -3,83 +3,14 @@ package main
 import (
 	"fmt"
 	"net"
-	"strings"
-	"sync"
 	"time"
+
+	nb "github.com/nexusriot/rezoagwe/pkg/node_bootstrap"
 )
-
-const (
-	broadcastPort = 9999
-	nodeTimeout   = 10000 * time.Second
-)
-
-type BootstrapNode struct {
-	mu    sync.Mutex
-	nodes map[string]time.Time
-}
-
-func NewBootstrapNode() *BootstrapNode {
-	return &BootstrapNode{
-		nodes: make(map[string]time.Time),
-	}
-}
-
-func (bn *BootstrapNode) RegisterNode(address string) {
-	bn.mu.Lock()
-	defer bn.mu.Unlock()
-	bn.nodes[address] = time.Now()
-	fmt.Printf("Registred node: %s\n", address)
-	fmt.Printf("nodes: %s\n", bn.nodes)
-}
-
-func (bn *BootstrapNode) RemoveStaleNodes() {
-	bn.mu.Lock()
-	defer bn.mu.Unlock()
-	now := time.Now()
-	for address, timestamp := range bn.nodes {
-		if now.Sub(timestamp) > nodeTimeout {
-			delete(bn.nodes, address)
-		}
-	}
-}
-
-func (bn *BootstrapNode) GetNodes() []string {
-	bn.mu.Lock()
-	defer bn.mu.Unlock()
-	nodes := []string{}
-	fmt.Printf("Cur nodes: %s\n", bn.nodes)
-	for address := range bn.nodes {
-		nodes = append(nodes, address)
-	}
-	return nodes
-}
-
-func handleBootstrap(bn *BootstrapNode, conn *net.UDPConn) {
-	buf := make([]byte, 1024)
-	for {
-		n, addr, err := conn.ReadFromUDP(buf)
-		if err != nil {
-			fmt.Println("Error reading from UDP:", err)
-			continue
-		}
-
-		message := strings.TrimSpace(string(buf[:n]))
-		if message == "DISCOVER" {
-			nodes := bn.GetNodes()
-			response := strings.Join(nodes, ",")
-			conn.WriteToUDP([]byte(response), addr)
-		} else if strings.HasPrefix(message, "REGISTER:") {
-			nodeAddress := strings.TrimPrefix(message, "REGISTER:")
-			bn.RegisterNode(nodeAddress)
-			conn.WriteToUDP([]byte("REGISTERED"), addr)
-			fmt.Printf("Nodes: %s\n", bn.GetNodes())
-		}
-	}
-}
 
 func main() {
 	addr := net.UDPAddr{
-		Port: broadcastPort,
+		Port: nb.BroadcastPort,
 		IP:   net.ParseIP("0.0.0.0"),
 	}
 	conn, err := net.ListenUDP("udp", &addr)
@@ -89,14 +20,14 @@ func main() {
 	}
 	defer conn.Close()
 
-	bn := NewBootstrapNode()
+	bn := nb.NewBootstrapNode()
 	go func() {
 		for {
 			bn.RemoveStaleNodes()
-			time.Sleep(nodeTimeout / 2)
+			time.Sleep(nb.NodeTimeout / 2)
 		}
 	}()
 
-	fmt.Printf("Bootstrap node is listening on port %d\n", broadcastPort)
-	handleBootstrap(bn, conn)
+	fmt.Printf("Bootstrap node is listening on port %d\n", nb.BroadcastPort)
+	nb.HandleBootstrap(bn, conn)
 }
--- pkg/node_bootstrap/bootstrap.go
+++ pkg/node_bootstrap/bootstrap.go
@@ -1,1 +1,78 @@
 package node_bootstrap
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	BroadcastPort = 9999
+	NodeTimeout   = 10000 * time.Second
+)
+
+type BootstrapNode struct {
+	mu    sync.Mutex
+	nodes map[string]time.Time
+}
+
+func NewBootstrapNode() *BootstrapNode {
+	return &BootstrapNode{
+		nodes: make(map[string]time.Time),
+	}
+}
+
+func (bn *BootstrapNode) RegisterNode(address string) {
+	bn.mu.Lock()
+	defer bn.mu.Unlock()
+	bn.nodes[address] = time.Now()
+	fmt.Printf("Registred node: %s\n", address)
+	fmt.Printf("nodes: %s\n", bn.nodes)
+}
+
+func (bn *BootstrapNode) RemoveStaleNodes() {
+	bn.mu.Lock()
+	defer bn.mu.Unlock()
+	now := time.Now()
+	for address, timestamp := range bn.nodes {
+		if now.Sub(timestamp) > NodeTimeout {
+			delete(bn.nodes, address)
+		}
+	}
+}
+
+func (bn *BootstrapNode) GetNodes() []string {
+	bn.mu.Lock()
+	defer bn.mu.Unlock()
+	nodes := []string{}
+	fmt.Printf("Cur nodes: %s\n", bn.nodes)
+	for address := range bn.nodes {
+		nodes = append(nodes, address)
+	}
+	return nodes
+}
+
+func HandleBootstrap(bn *BootstrapNode, conn *net.UDPConn) {
+	buf := make([]byte, 1024)
+	for {
+		n, addr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("Error reading from UDP:", err)
+			continue
+		}
+
+		message := strings.TrimSpace(string(buf[:n]))
+		if message == "DISCOVER" {
+			nodes := bn.GetNodes()
+			response := strings.Join(nodes, ",")
+			conn.WriteToUDP([]byte(response), addr)
+		} else if strings.HasPrefix(message, "REGISTER:") {
+			nodeAddress := strings.TrimPrefix(message, "REGISTER:")
+			bn.RegisterNode(nodeAddress)
+			conn.WriteToUDP([]byte("REGISTERED"), addr)
+			fmt.Printf("Nodes: %s\n", bn.GetNodes())
+		}
+	}
+}
